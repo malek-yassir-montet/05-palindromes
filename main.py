c2013b305eb0b6ddeb9c4ec37f48c35ed4c14941
Gustavo Jesus <gustavo>
2024-11-14 11:04:35 +0100

    script palindromes

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-#### Fonction secondaire
+"""Module pour importer l'alphabet minuscule et les nombres"""
+from string import ascii_lowercase, digits
 
+def ispalindrome(s):
+    """Fonction qui vérifie si un nombre est un palindrome"""
+    accents = "àâéèêëîïùûüôöç"
+    table = str.maketrans(accents, "aaeeeeiiuuuooc")
+    nouveau = ""
+    for i in s.lower().translate(table).lower():
+        if i in ascii_lowercase + digits:
+            nouveau += i
 
-def ispalindrome(p):
-
-    # votre code ici
-    
+    if nouveau == nouveau[::-1]:
+        return True
     return False
-
-#### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Fonction principale appelant la fonction palindrome"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
